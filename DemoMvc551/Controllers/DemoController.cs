7883bfeaa867040cd4799ad74d3cdf0ea1cf2e59
Duyvip-14 <duylun070502@gmail.com>
2025-11-04 10:59:04 +0700

    Bài thực hành số 3

---
+++ DemoMvc551/Controllers/DemoController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+namespace DemoMvc551.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+    }
